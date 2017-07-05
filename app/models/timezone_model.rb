f9e7a3516ecd4c7442b9115da0ba3bab27462cb5
Tiago Casanova <tiago.casanova@jumpseller.com>
2017-07-05 14:25:23 +0000

    Add TimezoneModel

---
+++ app/models/timezone_model.rb
@@ -0,0 +1,15 @@
+# Generic Swagger error class
+class TimezoneModel
+  include Swagger::Blocks
+
+  swagger_schema :TimezoneModel do
+    key :required, %i[timezone offset]
+    property :timezone do
+      key :type, :string
+    end
+    property :offset do
+      key :type, :integer
+      key :format, :int32
+    end
+  end
+end
