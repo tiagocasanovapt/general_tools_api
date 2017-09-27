8e04dbe157e509f46ec4e4faa68322859313071b
Tiago Casanova <tiago.casanova@jumpseller.com>
2017-09-28 00:48:51 +0100

    Update Timezones parameters with new format

---
+++ app/controllers/v1/docs/parameters/timezones.rb
@@ -0,0 +1,52 @@
+module V1
+  module Docs
+    module Parameters
+      # Documentation for Timezones Parameters
+      class Timezones
+        class << self
+          def city
+            proc do
+              key :name, :city
+              key :in, :path
+              key :description, 'Name of city to fetch'
+              key :required, true
+              key :type, :string
+            end
+          end
+
+          def country
+            proc do
+              key :name, :country
+              key :in, :path
+              key :description, 'Name / Code of country to fetch'
+              key :required, true
+              key :type, :string
+            end
+          end
+
+          def latitude
+            proc do
+              key :name, :lat
+              key :in, :query
+              key :description, 'Latitude of location'
+              key :required, true
+              key :type, :integer
+              key :format, :int64
+            end
+          end
+
+          def longitude
+            proc do
+              key :name, :lng
+              key :in, :query
+              key :description, 'Longitude of location'
+              key :required, true
+              key :type, :integer
+              key :format, :int64
+            end
+          end
+        end
+      end
+    end
+  end
+end
