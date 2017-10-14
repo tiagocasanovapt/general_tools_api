d064e19f8dcbdfba1c7c216f74c567c5744ff574
Tiago Casanova <tiago.casanova@jumpseller.com>
2017-10-14 16:03:57 +0100

    Add spec filters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,10 @@
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter 'app/controllers/apidocs_controller.rb'
+  add_filter 'app/controllers/v1/docs'
+  add_filter 'app/models'
+end
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
-class ActiveSupport
-  class TestCase
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-
-    # Add more helper methods to be used by all tests here...
-  end
-end
