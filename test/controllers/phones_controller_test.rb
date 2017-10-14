75225309d969c37609a856a98216c7f25cf6dec4
Tiago Casanova <tiago.casanova@jumpseller.com>
2017-10-14 17:16:21 +0100

    Spec for phones controller

---
+++ test/controllers/phones_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class PhonescontrollerTest < ActionDispatch::IntegrationTest
+  test 'should get the country phone call prefix' do
+    get '/v1/phones/country/PT'
+    assert_response :success
+
+    assert_not_nil(response.body)
+
+    result = JSON.parse(response.body)
+    assert_equal result.dig('call_prefix'), '351'
+  end
+end
