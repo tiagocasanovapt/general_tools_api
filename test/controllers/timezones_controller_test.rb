2f759cb28fd8b3ab6fed2cae32209cc887e79d0c
Tiago Casanova <tiago.casanova@jumpseller.com>
2017-10-14 16:04:09 +0100

    Timezones GPS controller spec

--- test/controllers/timezones_controller_test.rb
+++ test/controllers/timezones_controller_test.rb
@@ -1,6 +1,18 @@
+require 'test_helper'
+
 class TimezonesControllerTest < ActionDispatch::IntegrationTest
   test 'should get city timezone' do
     get '/v1/timezones/city/Porto'
+    assert_response :success
+
+    assert_not_nil(response.body)
+
+    result = JSON.parse(response.body)
+    assert_equal result['timezone'], 'Europe/Lisbon'
+  end
+
+  test 'should get GPS timezone' do
+    get '/v1/timezones/gps?lat=41.149968&&lng=-8.610243'
     assert_response :success
 
     assert_not_nil(response.body)
